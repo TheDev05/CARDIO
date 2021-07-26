0bae7f93ae4db053738b8b9b8177889de11280b1
Ankit Devanshi <a.singh725098@gmail.com>
2021-07-26 23:39:31 +0530

    CodeChef: Prctice

--- SandBox/CP_Template.cpp
+++ SandBox/CP_Template.cpp
@@ -1,36 +1,22 @@
 /* Hello Stalker!,
  * Just Believe in Yourself, Its Okay to Inspire/ Learn from others' code but never copy them,
- * With God's Grace, Let's Start!
+ * With God's Grace, Let's Begin this journey!
 */
 
 #include <iostream>
-
 int main()
 {
+    int t;
+    std::cin >> t;
 
-    int n;
-    std::cin >> n;
-
-    int ev_count = 0, od_count = 0;
-    for (int i = 0; i < n; i++)
+    while (t--)
     {
-        int val;
-        std::cin >> val;
+        int n;
+        std::cin >> n;
 
-        if (val % 2 == 0)
-        {
-            ev_count++;
-        }
-        else
-            od_count++;
-    }
+        n = n / 2;
+        n = n - 1;
 
-    if (ev_count > od_count)
-    {
-        std::cout << "READY FOR BATTLE" << '\n';
-    }
-    else
-    {
-        std::cout << "NOT READY" << '\n';
+        std::cout << (n * (n + 1)) / 2 << '\n';
     }
 }
