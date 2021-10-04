66668fdba5675245bff4dd0a5f33f9129bef6aa0
Ankit Devanshi <a.singh725098@gmail.com>
2021-10-04 23:56:20 +0530

    Added new item

---
+++ DSA/Algos/SlidingWindow01.cpp
@@ -0,0 +1,168 @@
+/*** Hello Stalker!,
+  * Just Believe in Yourself, Its Okay to Inspire/ Learn from others' code but never copy them,
+  * With Gods' Grace, Let's begin today's journey!
+  * Username: at all platform: TheDev05, except CodeChef: WhoCares05; Happy Coding >_<
+ ***/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Limits */
+#define imax INT_MAX
+#define imin INT_MIN
+
+/* Pairs */
+#define pi pair<ll, ll>
+#define ps pair<string, string>
+#define pis pair<ll, string>
+#define psi pair<string, ll>
+#define mp make_pair
+#define f first
+#define s second
+
+/* Maps */
+#define mpi map<ll, ll>
+#define mps map<string, string>
+#define mpis map<ll, string>
+#define mpsi map<string, ll>
+#define mpic map<ll, char>
+#define mpci map<char, ll>
+
+/* Vectors */
+#define vi vector<ll>
+#define vs vector<string>
+#define vpi vector<pair<ll, ll>>
+#define vvi vector<vector<ll>>
+#define pb emplace_back
+#define ppb pop_back
+
+/* Extras */
+#define en cout << '\n' // NewLine
+#define ll long long
+#define maxin(v) *max_element(v.begin(), v.end())
+#define minin(v) *min_element(v.begin(), v.end())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.begin(), v.end(), greater<ll>())
+#define loop(x, n) for (ll i = x; i < n; ++i)
+#define db(x) cout << #x << "[" << x << "]" << '\n';
+#define xx cout << "Executed Succesfully" << '\n'
+
+clock_t startTime;
+double getCurrentTime()
+{
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+void solve();
+
+const ll maxlimit = 1e7 + 10;
+vector<bool> isPrime(maxlimit, 1);
+
+void fillPrimes()
+{
+	isPrime[0] = isPrime[1] = false;
+	for (ll i = 2; i < maxlimit; i++)
+	{
+		if (isPrime[i] == true)
+		{
+			for (int j = 2 * i; j < maxlimit; j += i)
+			{
+				isPrime[j] = false;
+			}
+		}
+	}
+}
+
+int main()
+{
+	startTime = clock();
+
+	ios_base::sync_with_stdio(false);
+	std::cout << std::setprecision(10);
+	std::cout << std::fixed;
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	srand(time(NULL));
+
+	/* Input-Output data from text file */
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+
+	// fillPrimes();
+
+	int test = 1;
+	std::cin >> test;
+
+	while (test--)
+	{
+		solve();
+	}
+
+	double sec = getCurrentTime();
+	// cout << "\n"<< sec << "\n";
+}
+
+/* Check: Single testCase or muntiple */
+
+void solve()
+{
+	/* Given an array A[] of size N and a positive integer K, find the first negative integer for each and every window(contiguous subarray) of size K.
+
+	https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1#
+    */
+
+	/* Modification in sliding window problem */
+
+	vector<int> neg, result;
+	for (int i = 0; i < K; i++)
+	{
+		if (A[i] < 0)
+		{
+			neg.push_back(A[i]);
+		}
+	}
+
+	if (neg.size() != 0)
+	{
+		result.push_back(neg[0]);
+	}
+	else
+		result.push_back(0);
+
+	int index = 0;
+	for (int i = K; i < N; i++)
+	{
+		if (A[i] < 0)
+		{
+			neg.push_back(A[i]);
+		}
+
+		if (A[index] < 0 && neg.size() != 0)
+		{
+			A.erase(0);
+		}
+
+		if (neg.size() != 0)
+		{
+			result.push_back(neg[0]);
+		}
+		else
+			result.push_back(0);
+
+		index++
+	}
+
+	return (result);
+}
+
+/* Reminder:
+1. Check Corner Cases, Least Input & Max Input.
+2. Check for Inputs: 0, 1, 2, 3 & n-2, n-1, n.
+3. Long Long vs Int vs unsigned.
+4. All input equal, Input Repeated, Negative.
+5. Time Complexity.
+6. Float-Double Precisions.
+*/
