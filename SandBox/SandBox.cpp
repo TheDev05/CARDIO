225c9232f0635c4c86d4f0f6d3bcaff5761bf7d0
Ankit Devanshi <a.singh725098@gmail.com>
2021-10-07 21:33:23 +0530

    Practice: Dynamic size SWT

--- SandBox/SandBox.cpp
+++ SandBox/SandBox.cpp
@@ -3,5 +3,35 @@ using namespace std;
 
 int main()
 {
-   
+    int N, K;
+    std::cin >> N >> K;
+
+    vector<int> arr;
+
+    for (int i = 0; i < N; i++)
+    {
+        int avl;
+        std::cin >> avl;
+
+        arr.push_back(avl);
+    }
+
+    int index = 0, result = 0;
+    sort(arr.begin(), arr.end());
+
+    while (K > 0 && index < N)
+    {
+        K = K - arr[index];
+
+        if (K >= 0)
+        {
+            result++;
+        }
+
+        index++;
+    }
+
+    return (result);
+
+    // std::cout << result;
 }
