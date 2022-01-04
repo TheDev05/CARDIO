4d9c9ccd2824bcf23ae9fc6b0eabf1bb2afa2f74
Ankit Devanshi <a.singh725098@gmail.com>
2022-01-04 16:29:48 +0530

    Practice

--- A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
+++ A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
@@ -1,8 +1,8 @@
 /*
- Believe-In-your-Karma & chant with me,
+ Believe-In-Karma & chant with me,
 
-    "Hare Rama Hare Rama, Rama Rama Hare Hare,
-	 Hare Krishna Hare Krishna, Krishna Krishna Hare Hare !!"
+    "Hare Krishna Hare Krishna, Krishna Krishna Hare Hare,
+     Hare Rama Hare Rama, Rama Rama Hare Hare !!"
 
  Username: TheDev05, Happy Coding <3
 */
@@ -63,46 +63,73 @@ vector<bool> isPrime(maxlimit, 1);
 
 void fillPrimes()
 {
-	isPrime[0] = isPrime[1] = false;
-	for (ll i = 2; i < maxlimit; i++)
-	{
-		if (isPrime[i] == true)
-		{
-			for (int j = 2 * i; j < maxlimit; j += i)
-			{
-				isPrime[j] = false;
-			}
-		}
-	}
+    isPrime[0] = isPrime[1] = false;
+    for (ll i = 2; i < maxlimit; i++)
+    {
+        if (isPrime[i] == true)
+        {
+            for (int j = 2 * i; j < maxlimit; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	std::cout << std::setprecision(10);
-	std::cout << std::fixed;
+    ios_base::sync_with_stdio(false);
+    std::cout << std::setprecision(10);
+    std::cout << std::fixed;
 
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	// freopen("input.txt", "r", stdin);
-	// freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-	// fillPrimes();
+    // fillPrimes();
 
-	int testCase = 1;
-	// std::cin >> testCase;
+    int testCase = 1;
+    // std::cin >> testCase;
 
-	while (testCase--)
-	{
-		solve();
-	}
+    while (testCase--)
+    {
+        solve();
+    }
 }
 
 /* Check: Single testCase or muntiple */
 
 void solve()
 {
+    int n;
+    std::cin >> n;
+
+    if (n == 0)
+    {
+        std::cout << "1\n";
+        rn;
+    }
+
+    int val = n % 4;
+
+    if (val == 0)
+    {
+        std::cout << "6\n";
+    }
+    else if (val == 3)
+    {
+        std::cout << "2\n";
+    }
+    else if (val == 2)
+    {
+        std::cout << "4\n";
+    }
+    else if (val == 1)
+    {
+        std::cout << "8\n";
+    }
 }
 
 /* Reminder:
--- A_The_number_of_positions.cpp
+++ A_The_number_of_positions.cpp
@@ -2,7 +2,7 @@
  Believe-In-your-Karma & chant with me,
 
     "Hare Rama Hare Rama, Rama Rama Hare Hare,
-	 Hare Krishna Hare Krishna, Krishna Krishna Hare Hare !!"
+     Hare Krishna Hare Krishna, Krishna Krishna Hare Hare !!"
 
  Username: TheDev05, Happy Coding <3
 */
@@ -63,46 +63,63 @@ vector<bool> isPrime(maxlimit, 1);
 
 void fillPrimes()
 {
-	isPrime[0] = isPrime[1] = false;
-	for (ll i = 2; i < maxlimit; i++)
-	{
-		if (isPrime[i] == true)
-		{
-			for (int j = 2 * i; j < maxlimit; j += i)
-			{
-				isPrime[j] = false;
-			}
-		}
-	}
+    isPrime[0] = isPrime[1] = false;
+    for (ll i = 2; i < maxlimit; i++)
+    {
+        if (isPrime[i] == true)
+        {
+            for (int j = 2 * i; j < maxlimit; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	std::cout << std::setprecision(10);
-	std::cout << std::fixed;
+    ios_base::sync_with_stdio(false);
+    std::cout << std::setprecision(10);
+    std::cout << std::fixed;
 
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	// freopen("input.txt", "r", stdin);
-	// freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
-	// fillPrimes();
+    // fillPrimes();
 
-	int testCase = 1;
-	// std::cin >> testCase;
+    int testCase = 1;
+    // std::cin >> testCase;
 
-	while (testCase--)
-	{
-		solve();
-	}
+    while (testCase--)
+    {
+        solve();
+    }
 }
 
 /* Check: Single testCase or muntiple */
 
 void solve()
 {
+
+    int n, a, b;
+    std::cin >> n >> a >> b;
+
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int back = abs(i - 1);
+        int front = abs(n - i);
+
+        if (front >= a && back <= b)
+        {
+            count++;
+        }
+    }
+
+    std::cout << count << '\n';
 }
 
 /* Reminder:
