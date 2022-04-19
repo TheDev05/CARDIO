d56b9855ebc6bfc288ea85297463fafbd5f1292b
Ankit Devanshi <a.singh725098@gmail.com>
2022-04-19 22:06:39 +0530

    Practice

---
+++ C_Lose_it.cpp
@@ -0,0 +1,304 @@
+/*
+ Believe in your Karma & Keep chanting,
+
+    "Hare Rama Hare Rama, Rama Rama Hare Hare,
+     Hare Krishna Hare Krishna, Krishna Krishna Hare Hare !!"
+
+ Username: TheDev05, Happy Coding. <3
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define imax INT_MAX
+#define imin INT_MIN
+
+#define pi pair<ll, ll>
+#define ps pair<string, string>
+#define pis pair<ll, string>
+#define psi pair<string, ll>
+#define mp make_pair
+#define f first
+#define s second
+
+#define mpi map<ll, ll>
+#define mps map<string, string>
+#define mpis map<ll, string>
+#define mpsi map<string, ll>
+#define mpic map<ll, char>
+#define mpci map<char, ll>
+
+#define vi vector<ll>
+#define vs vector<string>
+#define vpi vector<pair<ll, ll>>
+#define sumof(v) accumulate(v.begin(), v.end(), (ll)0);
+#define vvi vector<vector<ll>>
+#define pb emplace_back
+#define ppb pop_back
+
+#define en cout << '\n'
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rn return
+#define all(x) (x).begin(), (x).end()
+#define maxin(v) *max_element(v.begin(), v.end())
+#define minin(v) *min_element(v.begin(), v.end())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / gcd(a, b)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.begin(), v.end(), greater<ll>())
+#define loop(x, n) for (ll i = x; i < n; ++i)
+#define rloop(x, n) for (ll i = n - 1; i >= x; i--)
+#define db1(x) cout << #x << "[" << x << "]" << '\n';
+#define xx cout << "Good 😊" << '\n';
+
+const ll maxlimit = 1e7 + 10;
+vector<bool> isPrime(maxlimit, 1);
+
+bool isPalin(std::string text)
+{
+    ll n = text.size();
+    loop(0, n / 2) if (text[i] != text[n - 1 - i]) return false;
+    return true;
+}
+
+void fillPrimes()
+{
+    isPrime[0] = isPrime[1] = false;
+    loop(2, maxlimit)
+    {
+        if (isPrime[i])
+        {
+            for (int j = 2 * i; j < maxlimit; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+}
+
+struct MyComp
+{
+    // cmp() for set-pair, sort set by second value or index.
+    bool operator()(const pair<int, int> &x, const pair<int, int> &y) const
+    {
+        return x.second < y.second;
+    }
+
+    // Call with: set<pair<int, int>, MyComp> data;
+};
+
+#define db(x)          \
+    cerr << #x << " "; \
+    read(x);           \
+    cerr << endl;
+
+void read(ll t)
+{
+    cerr << t;
+}
+
+void read(int t) { cerr << t; }
+void read(string t) { cerr << t; }
+void read(char t) { cerr << t; }
+void read(double t) { cerr << t; }
+void read(bool t) { cerr << t; }
+
+template <class T, class V>
+void read(pair<T, V> p);
+
+template <class T>
+void read(vector<T> v);
+
+template <class T>
+void read(set<T> v);
+
+template <class T, class V>
+void read(map<T, V> v);
+
+template <class T>
+void read(multiset<T> v);
+
+template <class T, class V>
+void read(pair<T, V> p)
+{
+    cerr << " {";
+    read(p.first);
+    cerr << ",";
+    read(p.second);
+    cerr << "}";
+}
+
+template <class T>
+void read(vector<T> v)
+{
+    cerr << ": ";
+    for (T i : v)
+    {
+        read(i);
+        cerr << " ";
+    };
+}
+
+template <class T>
+void read(set<T> v)
+{
+    cerr << ": ";
+    for (T i : v)
+    {
+        read(i);
+        cerr << " ";
+    };
+}
+
+template <class T>
+void read(multiset<T> v)
+{
+    cerr << ": ";
+    for (T i : v)
+    {
+        read(i);
+        cerr << " ";
+    };
+}
+
+template <class T, class V>
+void read(map<T, V> v)
+{
+    cerr << ": ";
+    for (auto i : v)
+    {
+        read(i);
+        cerr << " ";
+    };
+}
+
+template <typename T1, typename T2>
+istream &operator>>(istream &istream, pair<T1, T2> &p)
+{
+    return (istream >> p.first >> p.second);
+}
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    for (auto &it : v)
+        cin >> it;
+    return istream;
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &ostream, const pair<T1, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const vector<T> &c)
+{
+    for (auto &it : c)
+        cout << it << " ";
+    return ostream;
+}
+
+void solve();
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    std::cout << std::setprecision(10);
+    std::cout << std::fixed;
+
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    // fillPrimes();
+
+    int testCase = 1, gcode = 1;
+    // std::cin >> testCase;
+
+    while (testCase--)
+    {
+        // std::cout << "Case #" << gcode << ":" << '\n';
+        // gcode++;
+
+        solve();
+    }
+}
+
+/* Rule'5: Never Give Up & Keep Hustling */
+/* Work hard in silence let your success be the noise */
+/* Rise, Grind and Repeat */
+
+void solve()
+{
+    // Reminder: TestCases are single/multiple?
+    // 🌻|Hare Krishna|🌻
+
+    int n;
+    std::cin >> n;
+
+    map<int, int> data;
+    map<int, set<int>> res;
+    vi num(n);
+
+    data[4] = 0;
+    data[8] = 0;
+    data[15] = 0;
+    data[16] = 0;
+    data[23] = 0;
+
+    loop(0, n)
+    {
+        std::cin >> num[i];
+
+        data[num[i]]++;
+        res[num[i]].insert(i);
+    }
+
+    int min = imax;
+    for (auto i : data)
+    {
+        min = std::min(min, i.second);
+    }
+
+    if (min == 0)
+    {
+        std::cout << n << '\n';
+        rn;
+    }
+
+    // db(res);
+    // db(min);
+
+    int count = 0;
+    for (int i = 0; i < min; i++)
+    {
+        auto four = res[4].begin();
+        auto eight = res[8].upper_bound((*four));
+        auto onefive = res[15].upper_bound((*eight));
+        auto onesix = res[16].upper_bound((*onefive));
+        auto twothree = res[23].upper_bound((*onesix));
+        auto fourtwo = res[42].upper_bound((*twothree));
+
+        if (eight == res[8].end() || onefive == res[15].end() || onesix == res[16].end() || twothree == res[23].end() || fourtwo == res[42].end())
+        {
+            break;
+        }
+        else
+        {
+            count++;
+
+            res[4].erase(four);
+            res[8].erase(eight);
+            res[15].erase(onefive);
+            res[16].erase(onesix);
+            res[23].erase(twothree);
+            res[42].erase(fourtwo);
+        }
+    }
+
+    // db(count);
+
+    std::cout << n - (count * 6) << '\n';
+}
