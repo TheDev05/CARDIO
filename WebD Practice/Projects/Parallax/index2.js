61238d536e8630a9b745f3218f08014d694f6c38
TheDev05 <a.singh725098@gmail.com>
2023-01-17 17:26:27 +0530

    UPD

--- WebD Practice/Projects/Parallax/index2.js
+++
@@ -1,7 +0,0 @@
-let section = document.querySelector('section');
-window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  section.style.clipPath = "circle(" + value + " at center center)";
-});
-
-console.log("first");
