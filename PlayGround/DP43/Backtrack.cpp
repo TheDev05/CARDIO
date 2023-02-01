d56f25a8f390cf96ac91abecab4dcb9e4619cea7
TheDev05 <a.singh725098@gmail.com>
2023-02-02 01:42:24 +0530

    Practice

--- PlayGround/DP43/Backtrack.cpp
+++ PlayGround/DP43/Backtrack.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int getCount(std::vector<int> &num, std::vector<int> &temp, std::vector<int> &storage, int index)
+{
+    if (index >= num.size())
+    {
+        if (temp.size() > storage.size())
+            storage = temp;
+        return 0;
+    }
+
+    bool ok = true;
+    for (int i = 0; i < temp.size(); i++)
+    {
+        if (temp[i] % num[index] != 0 && num[index] % temp[i] != 0)
+            ok = false;
+    }
+
+    if (ok)
+    {
+        temp.push_back(num[index]);
+        getCount(num, temp, storage, index + 1);
+        temp.pop_back();
+    }
+
+    getCount(num, temp, storage, index + 1);
+    return 0;
+}
+
 int main()
 {
     int n;
@@ -12,5 +39,9 @@ int main()
         std::cin >> num[i];
     }
 
-    
+    std::vector<int> storage, temp;
+    getCount(num, temp, storage, 0);
+
+    for (auto i : storage)
+        std::cout << i << " ";
 }
