e5a3a27dbb284785ac262e2abfaa62e8c4285446
TheDev05 <a.singh725098@gmail.com>
2023-02-01 13:49:51 +0530

    Clean Ups

---
+++ PlayGround/DP43/Backtrack.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::vector<int> num(n);
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> num[i];
+    }
+
+    
+}
