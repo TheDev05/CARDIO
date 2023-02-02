e9a282fab1642389a2b27b007081a1cc23622e23
TheDev05 <a.singh725098@gmail.com>
2023-02-02 16:08:46 +0530

    Practice

---
+++ PlayGround/DP46/Backtrack.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getCount(std::vector<int> &num, std::map<int, int> &data, int count, int prevIndex, int index)
+{
+    if (index >= num.size())
+    {
+        data[count]++;
+        return 0;
+    }
+
+    if (prevIndex == 0 || num[index] > num[prevIndex - 1])
+        getCount(num, data, count + 1, index + 1, index + 1);
+    getCount(num, data, count, prevIndex, index + 1);
+
+    return 0;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::vector<int> num(n);
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> num[i];
+    }
+
+    // std::map<int, int> data;
+    // getCount(num, data, 0, 0, 0);
+
+    // auto it = data.end();
+    // it--;
+
+    // std::cout << it->second;
+
+    std::vector<int> storage(num.size(), 1);
+
+    int result = 0;
+    for (int i = 0; i < num.size(); i++)
+    {
+        int max = 0;
+        std::map<int, int> data;
+
+        for (int j = i - 1; j >= 0; j--)
+        {
+            data[storage[j]]++;
+            if (num[j] < num[i])
+                max = std::max(max, storage[j]);
+        }
+
+        storage[i] = 1 + max;
+        result = std::max(result, data[max]);
+    }
+
+    std::cout << result;
+
+    // for (auto i : data)
+    //     std::cout << i.first << " " << i.second << '\n';
+
+    // std::cout << data[result];
+}
