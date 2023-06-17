cd906a1de1db145e8b2caef8ba95dbbf64afe043
TheDev05 <a.singh725098@gmail.com>
2023-06-17 14:56:27 +0530

    Practice

--- PlayGround/48_Optimised.cpp
+++ PlayGround/48_Optimised.cpp
@@ -14,9 +14,24 @@ public:
     }
 };
 
-int generate(std::vector<int> &preorder, int &index, long long left, long long right)
+TreeNode *validate(std::vector<int> &preorder, int &index, long long left, long long right)
 {
+    if (index >= preorder.size())
+        return NULL;
 
+    if (preorder[index] > left && preorder[index] < right)
+    {
+        TreeNode *root = new TreeNode(preorder[index]);
+
+        index = index + 1;
+
+        root->left = validate(preorder, index, left, root->val);
+        root->right = validate(preorder, index, root->val, right);
+
+        return root;
+    }
+    else
+        return NULL;
 }
 
 int main()
@@ -29,5 +44,7 @@ int main()
         std::cin >> i;
 
     long long left = LONG_MIN, right = LONG_MAX;
-    generate(preorder, 0, left, right);
+    int index = 0;
+
+    std::cout << validate(preorder, index, left, right);
 }
