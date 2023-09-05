e290675a3a52c8d1967af2993e2bb6cbfc6f4619
TheDev05 <a.singh725098@gmail.com>
2023-09-05 22:40:13 +0530

    Practice

---
+++ LC515.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left, *right;
+
+    TreeNode(int temp)
+    {
+        this->left = this->right = NULL;
+        this->val = temp;
+    }
+};
+
+void traverse(TreeNode *root, std::vector<int> &res)
+{
+    if (root == NULL)
+        return;
+
+    std::queue<TreeNode *> inox;
+    inox.push(root);
+
+    while (inox.size())
+    {
+        int size = inox.size(), max = 0;
+        for (int i = 0; i < size; i++)
+        {
+            auto it = inox.front();
+            inox.pop();
+
+            if (it->left)
+                inox.push(it->left);
+            if (it->right)
+                inox.push(it->right);
+
+            max = std::max(max, it->val);
+        }
+
+        res.push_back(max);
+    }
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    std::vector<int> res;
+    traverse(root, res);
+
+    for (auto i : res)
+        std::cout << i << " ";
+}
