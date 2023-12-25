5a6e80d12dab6e9dc579029ff18f807a9dbe5653
TheDev05 <a.singh725098@gmail.com>
2023-12-25 23:25:16 +0530

    Practice

---
+++ LC91.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int traverse(std::string text, std::vector<int> &dp, int index)
+{
+    if (text[index] == '0')
+        return 0;
+    if (index >= text.size())
+        return 1;
+
+    if (dp[index] == -1)
+    {
+        int sum = 0, temp = 0;
+        for (int i = index; i < text.size(); i++)
+        {
+            int val = (int)(text[i] - '0');
+            temp = (temp * 10) + val;
+
+            if (temp < 27)
+                sum += traverse(text, dp, i + 1);
+            else
+                break;
+        }
+
+        dp[index] = sum;
+    }
+
+    return dp[index];
+}
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+
+    std::vector<int> dp(s.size() + 1, -1);
+    std::cout<< traverse(s, dp, 0);
+}
