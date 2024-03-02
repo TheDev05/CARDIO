d817d6be395e8a7fec32ab15e2b669c7035c201d
TheDev05 <a.singh725098@gmail.com>
2024-03-02 23:50:26 +0530

    Practice

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -2,6 +2,7 @@
 
 const num = [1, 2, 3, 4, 5];
 
+// For Each
 num.forEach((value) => {
   console.log(value);
 });
@@ -10,9 +11,28 @@ const temp = num.map((value, index) => {
   return value;
 });
 
+// Map
 temp.map((value) => {
   console.log(value);
 });
+
+// Filter
+const afterFilter = num.filter((value) => {
+  return value & 1;
+});
+
+console.log(afterFilter);
+
+// Map + Filter : Nested
+const res = num
+  .map((value, index) => {
+    return value * index;
+  })
+  .filter((value) => {
+    return value % 2 == 0;
+  });
+
+console.log(res);
 
 const data = ["ankit", "raj"];
 
@@ -20,6 +40,7 @@ data.map((value) => {
   console.log(`${value[0].toUpperCase()}${value.slice(1)}`);
 });
 
+// Slice by default, (begin, end)
 num.splice(2, 1);
 num.map((value) => console.log(value));
 
@@ -29,4 +50,8 @@ const obj = {
   age: 23,
 };
 
+//  For In : Iterate on keys (eg. used for Objects)
 for (const key in obj) console.log(obj[key]);
+
+// For of : Iterate on Values
+for (const value of data) console.log(value);
