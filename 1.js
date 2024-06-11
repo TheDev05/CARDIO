595270320a49b81222f3f916d6976f8028e2e3b5
TheDev05 <a.singh725098@gmail.com>
2024-06-11 13:20:33 +0530

    Practice

--- 1.js
+++ 1.js
@@ -2,43 +2,25 @@ document
   .getElementById("sendMessageBtn")
   .addEventListener("click", sendMessage);
 
-function sendMessage() {
-  const apiUrl = "https://graph.facebook.com/v19.0/303543622850608/messages";
-  const authToken =
-    "EAAGv1a6QRKkBO4RFHROMfZCWDMSLgLMP4JAhhRtGq5NZBcaNHMWSzZAtX1qqCaV4K9cScXuXJBPoKmFE0cnT7tZAZA6I6yGYytm7wejCbnVzEoq5ZAXqqTtoGXMZAcCcZA30AI89Fo4dv1qxPPZCh3RYk2doGoYZByg3NxZAYe714lMIDuJUSHAfc9vXPZAZAfh7xNSekr4AeZC5M4N5C3XP4QQdQZD";
+async function sendMessage() {
+  const apiUrl =
+    "http://cloud.smsindiahub.in/api/mt/SendSMS?APIKey=YzWjhJR5rkix0Af6SSo8gQ&senderid=WEBSMS&channel=Promo&DCS=0&flashsms=0&number=917764897552&text=test%20message&route=5&PEId=1";
 
-  const messageBody = {
-    messaging_product: "whatsapp",
-    recipient_type: "individual",
-    to: "+917764897552",
-    type: "text",
-    text: {
-      preview_url: false,
-      body: "auto matic",
-    },
-  };
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST", // You can change this to 'POST' if needed
+      headers: {
+        "Content-Type": "application/json", // Adjust headers if necessary
+      },
+    });
 
-  const fetchCall = async () => {
-    try {
-      const response = await fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(messageBody),
-      });
-
-      if (!response.ok) {
-        console.log(`HTTP error! status: ${response.status}`);
-      } else alert("message sent!");
-
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
+    if (!response.ok) {
+      throw new Error("Network response was not ok" + response.statusText);
     }
-  };
 
-  fetchCall();
+    const data = await response.json(); // Adjust this if the API returns non-JSON data
+    console.log(data);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
 }
